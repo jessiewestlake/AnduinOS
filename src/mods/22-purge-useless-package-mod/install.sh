42654b3229bf86d9b3c9bdac81e2feb028a9c820
Anduin Xue <anduin@aiursoft.com>
2025-04-10 13:55:46 +0000

    Fix install script to properly format package removal list

--- src/mods/22-purge-useless-package-mod/install.sh
+++ src/mods/22-purge-useless-package-mod/install.sh
@@ -22,7 +22,7 @@ apt purge -y \
     yaru-theme-unity yaru-theme-icon yaru-theme-gtk \
     apport python3-systemd \
     imagemagick* \
-    ubuntu-pro-client ubuntu-advantage-desktop-daemon ubuntu-advantage-tools ubuntu-pro-client ubuntu-pro-client-l10n
+    ubuntu-pro-client ubuntu-advantage-desktop-daemon ubuntu-advantage-tools ubuntu-pro-client ubuntu-pro-client-l10n \
     software-properties-gtk > /dev/null
 # Above remove everything about yaru-theme but keeps yaru-theme-sound and yaru-theme-gnome-shell (Required by Ubuntu session)
 judge "Purge unnecessary packages"
