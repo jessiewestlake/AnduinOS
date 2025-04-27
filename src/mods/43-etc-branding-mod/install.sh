8db5dd08ea591c2965389498865b46c053e1b203
Anduin Xue <anduin@aiursoft.com>
2025-04-27 14:04:08 +0000

    Update OS identification to 'ubuntu' for compatibility with Ubuntu features

--- src/mods/43-etc-branding-mod/install.sh
+++ src/mods/43-etc-branding-mod/install.sh
@@ -18,7 +18,7 @@ NAME="$TARGET_BUSINESS_NAME"
 VERSION_ID="$TARGET_BUILD_VERSION"
 VERSION="$TARGET_BUILD_VERSION ($TARGET_UBUNTU_VERSION)"
 VERSION_CODENAME=$TARGET_UBUNTU_VERSION
-ID=$TARGET_NAME
+ID=ubuntu
 ID_LIKE=debian
 HOME_URL="https://www.anduinos.com/"
 SUPPORT_URL="https://github.com/Anduin2017/AnduinOS/discussions"
@@ -26,4 +26,5 @@ BUG_REPORT_URL="https://github.com/Anduin2017/AnduinOS/issues"
 PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
 UBUNTU_CODENAME=$TARGET_UBUNTU_VERSION
 EOF
+# The ID have to be ubuntu to support some Ubuntu features, like add-apt-repository
 judge "Update os-release"
