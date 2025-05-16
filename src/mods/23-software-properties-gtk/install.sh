292b67b0f33a36c2d79f828c09668c9be380ed67
Anduin Xue <anduin@aiursoft.com>
2025-05-16 17:41:25 +0000

    Fix #74. Add software-properties-gtk.

---
+++ src/mods/23-software-properties-gtk/install.sh
@@ -0,0 +1,44 @@
+set -e                  # exit on error
+set -o pipefail         # exit on pipeline error
+set -u                  # treat unset variable as error
+
+print_ok "Downloading software-properties-gtk..."
+apt install -y python3-dateutil gir1.2-handy-1 --no-install-recommends
+judge "Install python3-dateutil"
+
+apt-get download "software-properties-gtk"
+judge "Download software-properties-gtk"
+
+DEB_FILE=$(ls *.deb)
+print_ok "Found $DEB_FILE"
+
+print_ok "Extracting $DEB_FILE..."
+mkdir original
+dpkg-deb -R "$DEB_FILE" original
+judge "Extract $DEB_FILE"
+
+print_ok "Patching control file..."
+sed -i \
+  '/^Depends:/s/, *ubuntu-pro-client//; /^Depends:/s/, *ubuntu-advantage-desktop-daemon//' \
+  original/DEBIAN/control
+judge "Edit control file"
+
+MOD_DEB="modified.deb"
+
+print_ok "Repackaging $MOD_DEB..."
+dpkg-deb -b original "$MOD_DEB"
+judge "Repackage $MOD_DEB"
+
+print_ok "Installing $MOD_DEB..."
+dpkg -i "$MOD_DEB"
+judge "Install $MOD_DEB"
+
+
+FILE=/usr/lib/python3/dist-packages/softwareproperties/gtk/SoftwarePropertiesGtk.py
+
+print_ok "Patching $FILE..."
+sudo cp "$FILE" "${FILE}.bak"
+sudo sed -i '/^from \.UbuntuProPage import UbuntuProPage$/d' "$FILE"
+sudo sed -i '/^[[:space:]]*def init_ubuntu_pro/,/^[[:space:]]*$/d' "$FILE"
+sudo sed -i '/^[[:space:]]*if is_current_distro_lts()/,/self.init_ubuntu_pro()/d' "$FILE"
+judge "Edit $FILE"
--- src/mods/78-no-advertisements-mod/install.sh
+++ src/mods/78-no-advertisements-mod/install.sh
@@ -2,10 +2,10 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
-print_ok "Removing Ubuntu Pro advertisements"
+print_ok "Ensuring Ubuntu Pro advertisement is disabled"
 FILE="/etc/apt/apt.conf.d/20apt-esm-hook.conf"
 if [[ -e "$FILE" ]]; then
   print_error "Error: $FILE exists, aborting."
   exit 1
 fi
-judge "Remove Ubuntu Pro advertisements"
+judge "Ensure Ubuntu Pro advertisement is disabled"
