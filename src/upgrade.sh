aa63b0037f1ad1fef524cb3d96b28299e80c02ba
Anduin Xue <anduin@aiursoft.com>
2025-05-18 14:27:59 +0000

    Add install_spg function to manage software-properties-gtk installation and patching

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -83,6 +83,66 @@ function upgrade_130_to_131() {
     judge "Upgrade from 1.3.0 to 1.3.1 completed"
 }
 
+function install_spg() {
+    print_ok "Downloading software-properties-gtk..."
+    sudo apt install -y \
+        python3-dateutil \
+        gir1.2-handy-1 \
+        libgtk3-perl \
+        --no-install-recommends
+    judge "Install python3-dateutil"
+
+    sudo apt-get download "software-properties-gtk"
+    judge "Download software-properties-gtk"
+
+    DEB_FILE=$(ls *.deb)
+    print_ok "Found $DEB_FILE"
+    sudo chown $USER:$USER "$DEB_FILE"
+
+    print_ok "Extracting $DEB_FILE..."
+    mkdir original
+    dpkg-deb -R "$DEB_FILE" original
+    judge "Extract $DEB_FILE"
+
+    print_ok "Patching control file..."
+    sed -i \
+    '/^Depends:/s/, *ubuntu-pro-client//; /^Depends:/s/, *ubuntu-advantage-desktop-daemon//' \
+    original/DEBIAN/control
+    judge "Edit control file"
+
+    MOD_DEB="modified.deb"
+
+    print_ok "Repackaging $MOD_DEB..."
+    dpkg-deb -b original "$MOD_DEB"
+    judge "Repackage $MOD_DEB"
+
+    print_ok "Cleaning up temp folder..."
+    rm -rf original
+
+    print_ok "Installing $MOD_DEB..."
+    sudo dpkg -i "$MOD_DEB"
+    judge "Install $MOD_DEB"
+
+    print_ok "Cleaning up $MOD_DEB and $DEB_FILE..."
+    rm -f "$MOD_DEB"
+    rm -f "$DEB_FILE"
+    judge "Clean up $MOD_DEB and $DEB_FILE"
+
+    FILE=/usr/lib/python3/dist-packages/softwareproperties/gtk/SoftwarePropertiesGtk.py
+
+    print_ok "Patching $FILE... to disable Ubuntu Pro"
+    sudo cp "$FILE" "${FILE}.bak"
+    sudo sed -i '/^from \.UbuntuProPage import UbuntuProPage$/d' "$FILE"
+    sudo sed -i '/^[[:space:]]*def init_ubuntu_pro/,/^[[:space:]]*$/d' "$FILE"
+    sudo sed -i '/^[[:space:]]*if is_current_distro_lts()/,/self.init_ubuntu_pro()/d' "$FILE"
+    judge "Edit $FILE"
+
+    print_ok "Marking software-properties-gtk as held..."
+    sudo apt-mark hold software-properties-gtk
+    judge "Mark software-properties-gtk as held"
+
+}
+
 function upgrade_131_to_132() {
     # If the flatpak remote is https://mirror.sjtu.edu.cn/flathub
     # Change it to sudo flatpak remote-modify flathub --url=https://mirrors.ustc.edu.cn/flathub
@@ -102,6 +162,8 @@ function upgrade_131_to_132() {
         vim \
         --no-install-recommends
     judge "Install vim completed"
+
+    apt list --installed | grep software-properties-gtk || install_spg
 }
 
 
