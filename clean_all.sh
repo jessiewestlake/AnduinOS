00141587ccb160338e72413895f0fe87c3b1d146
Anduin Xue <anduin@aiursoft.com>
2025-05-22 05:41:18 +0000

    Fix typo in clean_all.sh to correctly remove the new_building_os directory

--- clean_all.sh
+++ clean_all.sh
@@ -15,7 +15,7 @@ function clean_all() {
     sudo umount ./src/new_building_os/proc || sudo umount -lf ./src/new_building_os/proc || true
     sudo umount ./src/new_building_os/dev || sudo umount -lf ./src/new_building_os/dev || true
     sudo umount ./src/new_building_os/run || sudo umount -lf ./src/new_building_os/run || true
-    sudo rm -rf ./src/ew_building_os || true
+    sudo rm -rf ./src/new_building_os || true
     sudo rm -rf ./src/image || true
     sudo rm -rf ./src/dist || true
 }
