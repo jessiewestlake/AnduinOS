a1f35f7c4d4aa5f2b31eae4198013bffd194af49
Anduin Xue <anduin@aiursoft.com>
2025-05-25 13:29:28 +0000

    Update upgrade script for version 1.2.5 support

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -6,7 +6,7 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 export DEBIAN_FRONTEND=noninteractive
-export LATEST_VERSION="1.2.4"
+export LATEST_VERSION="1.2.5"
 export CODE_NAME="oracular"
 export OS_ID="AnduinOS"
 export CURRENT_VERSION=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2)
@@ -284,20 +284,27 @@ function main() {
               upgrade_121_to_122
               upgrade_122_to_123
               upgrade_123_to_124
+              upgrade_124_to_125
               ;;
           "1.2.1")
               upgrade_121_to_122
               upgrade_122_to_123
               upgrade_123_to_124
+              upgrade_124_to_125
               ;;
           "1.2.2")
               upgrade_122_to_123
               upgrade_123_to_124
+              upgrade_124_to_125
               ;;
           "1.2.3")
               upgrade_123_to_124
+              upgrade_124_to_125
               ;;
           "1.2.4")
+              upgrade_124_to_125
+              ;;
+          "1.2.5")
               print_ok "Your system is already up to date. No update available."
               exit 0
               ;;
