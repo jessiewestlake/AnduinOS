eb7704f71726b56eddc26001a04281eee4efff4c
Anduin Xue <anduin@aiursoft.com>
2025-05-30 11:48:01 +0000

    Add installation of librime-plugin-lua for ibus-rime users during upgrade to 1.3.3

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -219,6 +219,14 @@ function upgrade_132_to_133() {
       speech-dispatcher \
       espeak-ng-data \
       --no-install-recommends
+
+    # If ibus rime is installed, then install librime-plugin-lua
+    if dpkg -l | grep -q "ibus-rime"; then
+        print_ok "Installing librime-plugin-lua..."
+        sudo apt install -y librime-plugin-lua --no-install-recommends
+    else
+        print_warn "ibus-rime is not installed, skipping librime-plugin-lua installation."
+    fi
     judge "Upgrade from 1.3.2 to 1.3.3 completed"
 }
 
