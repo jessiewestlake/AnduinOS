0d8777cea0de2d68209015d24acee43fadb730cd
Florin Tanasa <florin.tanasa@svgenebank.ro>
2025-06-27 08:59:45 +0300

    Holds plymouth-theme-spinner to prevent upgrades
    Marks the plymouth-theme-spinner package as held to prevent upgrades, and creates a PIN file to ensure it is not upgraded. This is to prevent unwanted changes to the plymouth theme.

--- src/mods/19-plymouth-patch/install.sh
+++ src/mods/19-plymouth-patch/install.sh
@@ -11,3 +11,16 @@ cp ./anduinos_text.png /usr/share/plymouth/ubuntu-logo.png
 cp ./anduinos_text.png /usr/share/plymouth/themes/spinner/watermark.png
 #update-initramfs -u # We don't have to update initramfs here, because we did it in the end of this script
 judge "Patch plymouth and update initramfs"
+
+# hold theme spinner to be upgraded
+print_ok "Marking plymouth-theme-spinner as held..."
+apt-mark hold plymouth-theme-spinner
+judge "Mark plymouth-theme-spinner as held"
+
+print_ok "Marking plymouth-theme-spinner as not upgradeable..."
+cat << EOF > /etc/apt/preferences.d/no-upgrade-plymouth-theme-spinner
+Package: plymouth-theme-spinner
+Pin: release o=Ubuntu
+Pin-Priority: -1
+EOF
+judge "Create PIN file for plymouth-theme-spinner"
